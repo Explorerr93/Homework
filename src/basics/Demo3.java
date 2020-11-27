dd23ffed55d03c1aa683f8552c2ce86704970003
Explorerr93 <danusava@gmail.com>
2020-11-28 01:35:09 +0200

    git commit -m "furniture"

--- src/basics/Demo3.java
+++ src/basics/Demo3.java
@@ -2,11 +2,11 @@ package basics;
 
 public class Demo3 {
     public static void main(String... args ){
-        System.out.println("x\t\t\t\t\t\tx");
-        System.out.println("x\t\t\t\t\t\tx");
-        System.out.println("x\t\txxxxxxxxxx\t\tx");
-        System.out.println("xxxxx\tx\t\t x\txxxxx");
-        System.out.println("x   x\tx\t\t x\tx\tx");
-        System.out.println("x   x\tx\t\t x\tx\tx");
+        System.out.println("x                    x");
+        System.out.println("x                    x");
+        System.out.println("x      xxxxxxxx      x");
+        System.out.println("xxxxx  x      x xxxxxx");
+        System.out.println("x   x  x      x x    x");
+        System.out.println("x   x  x      x x    x");
     }
 }
