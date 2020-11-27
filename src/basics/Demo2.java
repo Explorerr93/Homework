ac426885b0c2465f724c809ffca1334e40a96775
Explorerr93 <danusava@gmail.com>
2020-11-28 00:54:20 +0200

    git commit -m "quote,movie,character,year of the movie"

---
+++ src/basics/Demo2.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Demo2 {
+    public static void main(String... args){
+        System.out.println("I will be back!");
+        System.out.println("Terminator parts: 1,2,3.");
+        System.out.println("Arnold Schwarzenegger");
+        System.out.println("Terminator parts 1, initial release: 1984");
+        System.out.println("Terminator parts 2, initial release: 1991");
+        System.out.println("Terminator parts 3, initial release: 2003");
+    }
+}
