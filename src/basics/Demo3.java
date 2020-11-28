df79b94dd9f7731d320765fc7508fb65e000f75a
Explorerr93 <danusava@gmail.com>
2020-11-28 02:00:45 +0200

    furniture

--- src/basics/Demo3.java
+++ src/basics/Demo3.java
@@ -1,8 +1,8 @@
 package basics;
 public class Demo3 {
     public static void main(String... args) {
-        System.out.println("x						x");
-        System.out.println("x						x");
+        System.out.println("x                       x");
+        System.out.println("x                       x");
         System.out.println("x      xxxxxxxxxx       x");
         System.out.println("xxxxx  x        x   xxxxx");
         System.out.println("x   x  x        x   x   x");
