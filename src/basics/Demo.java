b9b860079e6897f2e1192141b0955997ad04f776
Explorerr93 <danusava@gmail.com>
2020-11-30 20:02:38 +0200

    "furniture"

--- src/basics/Demo.java
+++ src/basics/Demo.java
@@ -1,7 +1,7 @@
 package basics;
 
 public class Demo {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         System.out.println("Never back down!");
     }
 
